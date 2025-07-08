fbbc0bf9288731c91bf05a32518e6076c1a6e8c9
niteeshkumarvissakoti <klu2100032045@gmail.com>
2025-07-08 12:08:23 +0530

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -3,13 +3,10 @@ package com.example;
 public class App {
     public static void main(String[] args) {
         Calculator calc = new Calculator();
-        System.out.println("Sum: " + calc.add(2, 3));
-        System.out.println("Product: " + calc.multiply(4, 5));
-    }
+        int sum = calc.add(5, 10);
+        int product = calc.multiply(5, 10);
 
-    // Method with long parameter list - common code smell
-    public void longParameterMethod(int a, int b, int c, int d, int e, int f) {
-        // Just a dummy implementation
-        System.out.println(a + b + c + d + e + f);
+        System.out.println("Sum: " + sum);
+        System.out.println("Product: " + product);
     }
 }
