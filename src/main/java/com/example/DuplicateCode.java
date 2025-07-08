3f90d88ebbf553bbf6dd5aaebc7b5f5ac668056d
niteeshkumarvissakoti <klu2100032045@gmail.com>
2025-07-08 12:46:03 +0530

    Update DuplicateCode.java

--- src/main/java/com/example/DuplicateCode.java
+++ src/main/java/com/example/DuplicateCode.java
@@ -4,10 +4,7 @@ public class DuplicateCode {
     public void printHello() {
         System.out.println("Hello, world!");
     }
-    public void printHello() {
-        System.out.println("Hello, world!");
-    }
-
+    
     // Duplicate method for SonarCloud to catch duplication
     public void printHelloAgain() {
         System.out.println("Hello, world!");
