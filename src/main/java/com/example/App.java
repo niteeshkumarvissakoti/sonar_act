42d496fe88ccf6782fed3f324dee4e9133f6b890
niteeshkumarvissakoti <klu2100032045@gmail.com>
2025-07-08 11:57:17 +0530

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -2,6 +2,14 @@ package com.example;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello from SonarCloud!");
+        Calculator calc = new Calculator();
+        System.out.println("Sum: " + calc.add(2, 3));
+        System.out.println("Product: " + calc.multiply(4, 5));
+    }
+
+    // Method with long parameter list - common code smell
+    public void longParameterMethod(int a, int b, int c, int d, int e, int f) {
+        // Just a dummy implementation
+        System.out.println(a + b + c + d + e + f);
     }
 }
