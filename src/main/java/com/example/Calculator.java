5cb15e5b632c8fe9098c1e630b50513b584d67e4
niteeshkumarvissakoti <klu2100032045@gmail.com>
2025-07-08 12:00:54 +0530

    Update Calculator.java

--- src/main/java/com/example/Calculator.java
+++ src/main/java/com/example/Calculator.java
@@ -1,6 +1,6 @@
 package com.example;
 
-public class App {
+public class Calculator {
     public static void main(String[] args) {
         Calculator calc = new Calculator();
         System.out.println("Sum: " + calc.add(2, 3));
